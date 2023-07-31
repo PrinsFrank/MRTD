1a4381b0146f5af54f97f4baad68155fab5979cf
Viktor Szépe <viktor@szepe.net>
2023-07-31 21:30:41 +0200

    Add PHP generics to MRZDataFactory::fromString

--- src/Data/Factory/MRZDataFactory.php
+++ src/Data/Factory/MRZDataFactory.php
@@ -3,7 +3,13 @@ declare(strict_types=1);
 
 namespace PrinsFrank\MRTD\Data\Factory;
 
+/**
+ * @template TMRZData of \PrinsFrank\MRTD\Data\MRZData
+ */
 interface MRZDataFactory
 {
+    /**
+     * @return TMRZData
+     */
     public static function fromString(string $string);
 }
