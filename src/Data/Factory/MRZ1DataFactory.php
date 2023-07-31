64321b76aa3fb433b0b797849666469dd1c1dd5f
PrinsFrank <25006490+PrinsFrank@users.noreply.github.com>
2023-07-31 09:16:42 +0200

    Implement MRZ1 specification

--- src/Data/Factory/MRZ1DataFactory.php
+++ src/Data/Factory/MRZ1DataFactory.php
@@ -4,11 +4,30 @@ declare(strict_types=1);
 namespace PrinsFrank\MRTD\Data\Factory;
 
 use PrinsFrank\MRTD\Data\MRZ3Data;
+use PrinsFrank\MRTD\DataElement\DataElementType;
+use PrinsFrank\MRTD\Definition\MRZ1Definition;
+use PrinsFrank\MRTD\Definition\MRZ3Definition;
+use PrinsFrank\MRTD\Support\StringParser;
 
 class MRZ1DataFactory implements MRZDataFactory
 {
     public static function fromString(string $string): MRZ3Data
     {
-        // TODO: Implement fromString() method.
+        $itemData = [];
+        foreach (MRZ3Definition::getDataElements() as $dataElement) {
+            $itemData[$dataElement->dataElementType->name] = StringParser::getChars($string, $dataElement->lineNumber, MRZ1Definition::nrOfCharactersPerLine(), $dataElement->charPosStart, $dataElement->charPosEnd);
+        }
+
+        return new MRZ3Data(
+            $itemData[DataElementType::DOCUMENT_CODE->name],
+            $itemData[DataElementType::ISSUING_STATE_OR_ORGANIZATION->name],
+            $itemData[DataElementType::NAME->name],
+            $itemData[DataElementType::PASSPORT_NUMBER->name],
+            $itemData[DataElementType::NATIONALITY->name],
+            $itemData[DataElementType::DATE_OF_BIRTH->name],
+            $itemData[DataElementType::SEX->name],
+            $itemData[DataElementType::DATE_OF_EXPIRY->name],
+            $itemData[DataElementType::PERSONAL_NUMBER_OR_OTHER_OPTIONAL_DATA_ELEMENTS->name]
+        );
     }
 }
--- src/Definition/MRZ1Definition.php
+++ src/Definition/MRZ1Definition.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 namespace PrinsFrank\MRTD\Definition;
 
 use PrinsFrank\MRTD\CheckDigit\CheckDigit;
+use PrinsFrank\MRTD\CheckDigit\CheckDigitCharacterRange;
 use PrinsFrank\MRTD\DataElement\DataElement;
+use PrinsFrank\MRTD\DataElement\DataElementType;
 
 class MRZ1Definition implements MRZDefinition
 {
@@ -21,12 +23,28 @@ class MRZ1Definition implements MRZDefinition
     /** @return array<DataElement> */
     public static function getDataElements(): array
     {
-        return [];
+        return [
+            new DataElement(DataElementType::DOCUMENT_CODE, 1, 1, 2),
+            new DataElement(DataElementType::ISSUING_STATE_OR_ORGANIZATION, 1, 3, 5),
+            new DataElement(DataElementType::DOCUMENT_NUMBER, 1, 6, 14),
+            new DataElement(DataElementType::OPTIONAL_DATA_ELEMENTS, 1, 16, 30),
+            new DataElement(DataElementType::DATE_OF_BIRTH, 2, 1,6),
+            new DataElement(DataElementType::SEX, 2, 8, 8),
+            new DataElement(DataElementType::DATE_OF_EXPIRY, 2, 9, 14),
+            new DataElement(DataElementType::NATIONALITY, 2, 16, 18),
+            new DataElement(DataElementType::OPTIONAL_DATA_ELEMENTS, 2, 19, 29),
+            new DataElement(DataElementType::NAME, 3, 1, 30),
+        ];
     }
 
     /** @return array<CheckDigit> */
     public static function getCheckDigits(): array
     {
-        return [];
+        return [
+//            new CheckDigit(1, 15, new CheckDigitCharacterRange(1, 6, 14)),
+            new CheckDigit(2, 7, new CheckDigitCharacterRange(2, 1, 6)),
+            new CheckDigit(2, 15, new CheckDigitCharacterRange(2, 9, 14)),
+            new CheckDigit(2, 30, new CheckDigitCharacterRange(1, 6, 30), new CheckDigitCharacterRange(2, 1, 7), new CheckDigitCharacterRange(2, 9, 15), new CheckDigitCharacterRange(2, 19, 29)),
+        ];
     }
 }
